c3c0656c0cc12f287729074f20bdfbc618ee5581
motioncloud <jonathan@motion.cloud>
2025-06-29 04:59:22 -0400

    Create sidebars.js

---
+++ sidebars.js
@@ -0,0 +1,14 @@
+// sidebars.js
+module.exports = {
+  tutorialSidebar: [
+    '1-executive-summary',
+    '2-project-overview',
+    '3-technical-foundations',
+    '4-step-by-step',
+    '5-why-it-matters',
+    '6-pricing-model',
+    '7-long-term-value',
+    '8-partnership-support',
+    '9-appendices',
+  ],
+};
